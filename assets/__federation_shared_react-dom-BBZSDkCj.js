b087974d582d6126c1290723fc1236ae68b4034c
Arfazappu <Arfazappu@users.noreply.github.com>
2025-05-12 07:59:32 +0000

    deploy: ef5991d4baf56da951591d8e16f6760541b08294

---
+++ assets/__federation_shared_react-dom-BBZSDkCj.js
@@ -0,0 +1,7 @@
+import { g as getDefaultExportFromCjs } from './_commonjsHelpers-BQ54P8Zj.js';
+import { r as requireReactDom } from './index-Dl6QBmMQ.js';
+
+var reactDomExports = requireReactDom();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
+
+export { index as default };
