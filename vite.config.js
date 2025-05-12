0fe7a80946b8bed21f1bd02a1c2fb63b1c893546
Arfaz Ahmed <arfazaar14@gmail.com>
2025-05-12 13:57:58 +0530

    added base in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/task-app/',
   plugins: [react(),
     federation({
       name:'taskApp',
