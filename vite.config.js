e2317a73a677ec344eec530100024307c8ced21b
Arfaz Ahmed <108565133+Arfazappu@users.noreply.github.com>
2025-05-13 13:52:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/task-app/',
+  base: '/task-remote-app/',
   plugins: [react(),
     federation({
       name:'taskApp',
